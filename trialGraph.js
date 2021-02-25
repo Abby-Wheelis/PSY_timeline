a338aa9bd961ef203e4de739910b5e879545f502
Abby-Wheelis <54848919+Abby-Wheelis@users.noreply.github.com>
2021-02-24 22:44:14 -0500

    PSY_things

--- trialGraph.js
+++ trialGraph.js
@@ -5,7 +5,7 @@ var TimelinePromis = d3.csv("TimelineCSV.csv");
 Promise.all([TimelinePromis]).then(
   function (data) {
     console.log("data", data);
-    timelineSetup();
+    timelineSetup(data);
   },
 
   function (err) {
@@ -22,11 +22,11 @@ and bam you did it girlie
 */
 
 /*
-//draws the circles and handles the mouseover
-var drawCircles = function(dataArray, xScale, yScale, rScale, cScale)
+//make school lil infos
+var layTimeline = function(dataArray, yScale)
 {
             
-    var spots = d3.select("#scatterplot")        
+    var school = d3.select("#scatterplot")        
     .selectAll("circle")
     .data(dataArray)
                         
@@ -68,8 +68,13 @@ var drawCircles = function(dataArray, xScale, yScale, rScale, cScale)
         })
 }
 */
+var getSchool = function (school) {
+  Name = school.School;
+  return Name;
+};
+
 /// begin extra SVG for the time slider dude down there
-var timelineSetup = function () {
+var timelineSetup = function (psyData) {
   //dimensions
   var tScreen = { width: 800, height: 1200 };
 
@@ -78,34 +83,70 @@ var timelineSetup = function () {
   var width = tScreen.width - tMargins.left - tMargins.right;
   var tHeight = tScreen.height - tMargins.top - tMargins.bottom;
 
-  var tScale = d3.scaleLinear().domain([1820, 2020]).range([0, tHeight]);
+  var tScale = d3.scaleLinear().domain([1830, 2020]).range([0, tHeight]);
 
   //axis
   var yAxis = d3.axisLeft().scale(tScale).tickFormat(d3.format("d")).ticks(16);
+    .attr("height", tScreen.height);
 
-  var svg = d3.select("#Time")
+  var svg = d3
+    .select("#Time")
     .append("svg")
     .attr("class", "timeline")
     .attr("width", tScreen.width)
     .attr("height", tScreen.height);
+    .call(yAxis); //need new axis setups
 
-  svg.append("g")
+  svg
+    .append("g")
     .attr("id", "Axis")
     .attr(
       "transform",
-      "translate(" + tMargins.left + "," + (tMargins.top + tHeight) + ")"
+      "translate(" + tMargins.left + "," + (tMargins.top + 15) + ")"
     )
     .call(yAxis); //need new axis setups
 
-  svg.append("text")
-    .attr(
-      "transform",
-      "translate(" +
-        (width + 100) / 2 +
-        "," +
-        (tHeight + tMargins.top + 30) +
-        ")"
-    )
+  svg
+    .append("text")
+    .attr("transform", "translate(" + tMargins.left + "," + tMargins.top + ")")
     .style("text-anchor", "middle")
     .text("Year");
-};
+
+  svg.append("g").attr("id", "Schools");
+
+  var Schools = d3
+    .select("#Schools")
+    .selectAll("g")
+    .data(psyData[0])
+    .enter()
+    .append("g")
+    .attr("transform", function (d) {
+      return "translate(55," + tScale(d.Start) + ")";
+    })
+    .attr("width", 50)
+    .attr("height", 20);
+
+  Schools.append("text").text(getSchool);
+
+  Schools.on("mouseover", function (d) {
+    var xPosition = d3.event.pageX; //add later to tweak
+    var yPosition = d3.event.pageY;
+
+    var tooltip = d3
+      .select("#tooltip")
+      .style("left", xPosition + "px")
+      .style("top", yPosition + "px")
+      .select("#name")
+      .text("Proponents: " + d.People);
+
+    var tooltip2 = d3
+      .select("#tooltip")
+      .select("#words")
+      .text("Description: " + d.Description);
+
+    var tooltip3 = d3.select("#tooltip").select("strong").text(d.School);
+
+    d3.select("#tooltip").classed("hidden", false);
+  }).on("mouseout", function () {
+    d3.select("#tooltip").classed("hidden", true);
+  });
